7e33c9b87306dff872d3ab4149c04a6f61664c62
Muhammad Rivki <muhammad.rivki@outlook.com>
2017-04-19 23:44:13 +0700

    Add styling complete todo

--- app/components/TodoLists.js
+++ app/components/TodoLists.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 import { Switch } from 'react-native';
 import { List, ListItem, Text } from 'native-base';
 
+const style = {
+  text: {
+    marginLeft: 10,
+  },
+  completed: {
+    marginLeft: 10,
+    textDecorationLine: 'line-through',
+    color: '#ddd',
+  },
+};
+
 // create a component
 const TodoLists = props => (
   <List>
@@ -11,7 +22,13 @@ const TodoLists = props => (
         value={props.isComplete}
         onValueChange={props.onToggleChange}
       />
-      <Text style={{ marginLeft: 10 }}>{props.text}</Text>
+      <Text
+        style={props.isComplete ?
+        style.completed :
+        style.text}
+      >
+        {props.text}
+      </Text>
     </ListItem>
   </List>
   );
